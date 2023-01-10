2b4d9128e110a33a33f5b9a7ad6cf4ec079c41c7
Mariana Werneck <marinhomariana8@gmail.com>
2023-01-10 12:13:34 -0300

    :feat: icon size 30px

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,18 +1,21 @@
 import { BsGithub, BsLinkedin, BsTelegram } from 'react-icons/bs';
+import { ListAbout, MainAbout } from '../styles/About';
 
 function About() {
   return (
-    <div>
+    <MainAbout>
       <h1>About me</h1>
-      <p>Hello there! My name is Mariana, i'm 21 years old and i'm currently learning back-end!</p>
-      <p>This front-end is consuming an API created by me, using mostly NodeJS, Sequelize and Express.</p>
-      <p>This is my first full-stack project done by myself, feel free to give me a feedback.</p>
       <div>
-        <a href="https://github.com/mariyzx/" target="_blank" rel="noreferrer"><BsGithub /></a>
-        <a href="https://www.linkedin.com/in/marinhomariana8/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
-        <a href="https://t.me/mariyzx" target="_blank" rel="noreferrer"><BsTelegram /></a>
+        <p>Hello there! My name is Mariana, i'm 21 years old and i'm currently learning back-end!</p>
+        <p>This front-end is consuming an API created by me, using mostly NodeJS, Sequelize and Express.</p>
+        <p>This is my first full-stack project done by myself, feel free to give me a feedback.</p>
       </div>
-    </div>
+      <ListAbout>
+        <a href="https://github.com/mariyzx/" target="_blank" rel="noreferrer"><BsGithub size={30} /></a>
+        <a href="https://www.linkedin.com/in/marinhomariana8/" target="_blank" rel="noreferrer"><BsLinkedin size={30} /></a>
+        <a href="https://t.me/mariyzx" target="_blank" rel="noreferrer"><BsTelegram size={30} /></a>
+      </ListAbout>
+    </MainAbout>
   )
 }
 
---
+++ frontend/src/styles/About.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const MainAbout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+
+`
+
+export const ListAbout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`
