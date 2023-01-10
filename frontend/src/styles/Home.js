7344d782bf2626597a02cfb0362abf493085cb5a
Mariana Werneck <marinhomariana8@gmail.com>
2023-01-10 12:17:16 -0300

    :feat: add poppins font

--- frontend/src/styles/Home.js
+++ frontend/src/styles/Home.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Main = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: grey;
   justify-content: center;
   align-items: center;
   gap: 7rem;
