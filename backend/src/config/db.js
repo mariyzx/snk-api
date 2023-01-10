f2dd926f3bc6b90abe90798f39957e5f4f9dbdaf
Mariana Werneck <marinhomariana8@gmail.com>
2023-01-09 21:17:24 -0300

    :feat: connecting server and database

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'mysql',
+  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+}
