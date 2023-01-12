07c2cf98a3228eb52b4a70703c1304ec3420a48b
Mariana Werneck <marinhomariana8@gmail.com>
2023-01-12 09:43:56 -0300

    :fix: add apis endpoint

--- frontend/src/services/request.js
+++ frontend/src/services/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3333/",
+  baseURL: "https://snk-api-production-c1d2.up.railway.app/",
 });
 
 export default api;
