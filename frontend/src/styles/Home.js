940ac7f3af1a1b006e1de64d94f38b6ca0b764de
Mariana Werneck <marinhomariana8@gmail.com>
2023-01-12 07:07:26 -0300

    :fix: adjust of homepage style

--- frontend/src/styles/Home.js
+++ frontend/src/styles/Home.js
@@ -8,12 +8,9 @@ export const Main = styled.div`
   text-align: center;
   padding: 1rem;
 
-  div {
-    width: 100%;
-  }
-
   h1 {
     width: 100%;
+    font-size: 50px;
 	  animation: focus-in-contract 1.3s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
   }
   
@@ -22,18 +19,28 @@ export const Main = styled.div`
     position: absolute;
   }
 
-  div > a {
+  div:nth-child(2) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 30%;
+  }
+
+  a {
     text-decoration: none;
     font-weight: bold;
     color: lightgrey;
     margin: 5px;
   }
-
+  
+  a:hover {
+    color: white;
+  }
   // animista
 
   @keyframes focus-in-contract {
     0% {
-      letter-spacing: 0.4em;
+      letter-spacing: 2px;
       -webkit-filter: blur(12px);
               filter: blur(12px);
       opacity: 0;
@@ -45,12 +52,17 @@ export const Main = styled.div`
     }
   }
 
-  @media (max-width: 500px) {
+  @media (max-width: 700px) {
       flex-direction: column;
       gap: 4rem;
 
+      div:nth-child(1) {
+        width: 90%;
+      }
+
       div:nth-child(2) {
         display: flex;
+        flex-direction: row;
         justify-content: center;
         width: 100%;
       }
