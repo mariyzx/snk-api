5769576956a081d5b320cde77f54e7f63055202f
Mariana Werneck <marinhomariana8@gmail.com>
2023-01-12 09:29:53 -0300

    :feat: add variables for deploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: 'mysql',
-  host: process.env.MYSQL_HOST,
-  username: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  dialect: 'postgres',
+  host: process.env.PGHOST,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
   define: {
     timestamps: true,
     underscored: true,
