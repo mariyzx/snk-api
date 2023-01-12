b51075853af1bcc243ff6e0245c8a092a78dabc7
Mariana Werneck <marinhomariana8@gmail.com>
2023-01-12 09:47:29 -0300

    :fix: adjust link to swagger doc

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Menu() {
   return (
     <div>
-      <a href="http://localhost:3333/docs" target="_blank" rel="noreferrer">Docs</a>
+      <a href="https://snk-api-production-c1d2.up.railway.app/docs/" target="_blank" rel="noreferrer">Docs</a>
       <Link to="/characters">Characters</Link>
       <Link to="/titans">Titans</Link>
       <Link to="/about">About</Link>
